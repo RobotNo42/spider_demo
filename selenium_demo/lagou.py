de883bbec6696cc0020518e1977023fb729a469a
RobotNo42 <chengge1124@gmail.com>
2020-03-23 01:05:08 +0800

    change demo

--- selenium_demo/lagou.py
+++ selenium_demo/lagou.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from lxml import etree
 
 
+job_list = []
 class LaGouSpider(object):
     driver_path = r'D:\chromedriver\chromedriver.exe'
 
@@ -11,9 +12,29 @@ class LaGouSpider(object):
 
     def run(self):
         self.driver.get(self.url)
-        print(self.driver.page_source)
+        self.parse_source_list(self.driver.page_source)
+
+    def parse_source_list(self, source):
+        page = etree.HTML(source)
+        urls = page.xpath("//ul[@class='item_con_list']//a[@class='position_link']/@href")
+        for u in urls:
+            self.request_detail_page(u)
+
+    def request_detail_page(self,url):
+        self.driver.get(url)
+        self.parse_page(self.driver.page_source)
+
+    def parse_page(self,page):
+        t = etree.HTML(page)
+        name = t.xpath("/html/body/div[6]/div/div[1]/div/h1/text()")[0].strip()
+        money = t.xpath("//dd[@class='job_request']/h3/span[1]/text()")[0].strip()
+        job_list.append({
+            'job_name': name,
+            'money': money
+        })
 
 
 if __name__ == '__main__':
     spider = LaGouSpider()
     spider.run()
+    print(job_list)
